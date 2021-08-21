dd7ef8f41f0239acd0326fd864679b1e8c00dacb
abbasam <tiggerwitht1378@gmail.com>
2021-08-21 17:52:36 +0430

    add components header And Footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,23 @@
 <script>
 	import Footer from './components/Footer.svelte';
 	import Header from "./components/header.svelte";
+	import Tabs from "./shared/Tabs.svelte";
+
+	//tabs
+	let items = ['نظرسنجی های فعلی','افزودن نظرسنجی جدید']
+	let activeItem = 'نظرسنجی های فعلی';
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
 </script>
 <Header />
 <main>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sit nam necessitatibus eligendi voluptatibus in earum corrupti voluptate excepturi, accusamus fugiat itaque tempora alias placeat quam! Alias nisi labore obcaecati!</p>
+	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
+	{#if activeItem === 'نظرسنجی های فعلی'}
+		<p>نظرسنجی فعلی</p>
+	{:else if activeItem == 'افزودن نظرسنجی جدید'}
+		<p>نظرسنجی جدید</p>
+	{/if}
 </main>
 <Footer/>
 <style>
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispath = createEventDispatcher();
+    export let items;
+    export let activeItem;
+</script>
+
+<div class="tabs">
+    <ul>
+        {#each items as item}
+            <li>
+                <div on:click={() => dispath('tabChange',item)} class:active={item === activeItem}>{item}</div>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+    .tabs{
+        margin-bottom: 40px;
+    }
+    ul{
+        display: flex;
+        justify-content: center;
+        margin-bottom: 40px;
+        padding: 0;
+        list-style-type: none;
+    }
+    li{
+        margin: 0 16px;
+        font-size: 18px;
+        color: rgb(158, 158, 158);
+        cursor: pointer;
+    }
+    .active{
+        color: #d83a5a;
+        border-bottom: 2px solid #d83a5a;
+    }
+</style>
